f9b33656a100576bd6d31a164124a64d528cdf8f
asharghi <a.sharghi.mail@gmail.com>
2024-08-28 12:32:27 +0200

    Set cli to 8.8.1

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -13,29 +13,52 @@
 <script lang="ts">
 var Star = require("nativescript-print-star");
 import Vue from "nativescript-vue";
+import {
+  check as permissionCheck,
+  request as requestPermission,
+} from "@nativescript-community/perms";
 
 export default Vue.extend({
   methods: {
+    async checkAndAskForPermissions() {
+      const comp = this;
+
+      const isAuthorized = (response) => {
+        if (Array.isArray(response)) return response.includes("authorized");
+        else if (typeof response === "object" && response !== null)
+          return Object.values(response).some(
+            (value) => value === "authorized"
+          );
+        return false;
+      };
+
+      const bluetoothResponse = await permissionCheck("bluetooth");
+      if (!isAuthorized(bluetoothResponse)) {
+        await requestPermission("bluetooth");
+      }
+
+      return true;
+    },
     tapped(args) {
       console.log("Button was pressed");
-      try {
-       (new Star.Discovery().searchBluetooth())
-        .then((res) => {
-          console.log("Success");
-        })
-        .catch((err) => {
-          console.log('-error');
-          console.log(err);
-        });
-      } catch (error) {
-        console.log('--error');
-        console.log(error);
-      }
-    },
-  },
-  computed: {
-    message() {
-      return "Blank {N}-Vue app";
+
+      this.checkAndAskForPermissions().then(() => {
+        try {
+          new Star.Discovery()
+            .searchBluetoothLE()
+            .then((res) => {
+              console.log(res);
+              console.log("Success");
+            })
+            .catch((err) => {
+              console.log("-error");
+              console.log(err);
+            });
+        } catch (error) {
+          console.log("--error");
+          console.log(error);
+        }
+      });
     },
   },
 });
